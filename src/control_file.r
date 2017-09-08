d32517e0209ef35cc1f96611aecd752881c3df9b
xavier-gutierrez <xaviersgutierrez@gmail.com>
2017-09-08 11:46:23 -0400

    Fixed directory paths

--- src/control_file.r
+++ src/control_file.r
@@ -5,6 +5,7 @@ install.packages('data.table')
 install.packages('mlogit')
 install.packages('ggplot2')
 install.packages('reshape2')
+install.packages('lubridate')
 options(scipen=999)
 require(R.utils)
 require(dplyr)
@@ -12,12 +13,13 @@ require(tidyr)
 require(data.table)
 require(ggplot2)
 require(reshape2)
+require(lubridate)
 require(mlogit)
 
-path_code = "C:/Users/bloh356/Documents/GitHub/fuzzy-waffle/src/"
-path_data = "C:/Users/bloh356/Documents/GitHub/fuzzy-waffle/data/"
-path_figure = "C:/Users/bloh356/Documents/GitHub/fuzzy-waffle/papers/figures/"
-#path_data = "/Users/bloh356/Documents/fuzzy-waffle/data/"
+path_code = "C:/Users/guti220/Desktop/energy_market_shares/src/"
+path_data = "C:/Users/guti220/Desktop/energy_market_shares/data/"
+path_data = "C:/Users/guti220/Desktop/fuzzy-waffle-download/data/"
+path_figure = "C:/Users/guti220/Desktop/energy_market_shares/figures/"
 # Local path to the cloned data and code folders in the repository
 
 source(paste0(path_code,"mapping_860_overnight_R.r"))
@@ -25,7 +27,7 @@ source(paste0(path_code,"mapping_860_overnight_R.r"))
 source(paste0(path_code, "ca_almanac_R.r"))
 source(paste0(path_code, "summary_files.r"))
 source(paste0(path_code, "overnight_cost.r"))
-source(paste0(path_code, 'multinomial.r')) # warning a dplyr function call will take ~ 6 minutes (instead just run the sections of code after it, 
-#       unless changes have been made to data tables)
+source(paste0(path_code, 'multinomial.r')) 
+# warning a dplyr function call will take ~ 6 minutes (instead just run the sections of code after it, unless changes have been made to data tables)
 source(paste0(path_code, "figure.r"))
 
